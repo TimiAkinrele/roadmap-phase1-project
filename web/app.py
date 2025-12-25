66fe79f187da9ee7b1a97d0c21972fd621bced6d
Timi Akinrele <timiakinrele@Timis-MacBook-Pro.local>
2025-12-25 22:52:40 +0000

    Adding sslmode for secure socket layer transport

--- web/app.py
+++ web/app.py
@@ -14,6 +14,7 @@ def get_db_connection():
                 database=os.environ.get('DB_NAME'),
                 user=os.environ.get('DB_USER'),
                 password=os.environ.get('DB_PASSWORD')
+                sslmode='require'
             )
             print("Database connection successful!")
             return conn
